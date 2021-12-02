5146023ee234dae61f8017a2512d400960b91478
schmelto <30869493+schmelto@users.noreply.github.com>
2021-12-02 18:24:11 +0100

    style: format file

--- src/day01/day01.js
+++ src/day01/day01.js
@@ -9,7 +9,7 @@ for (var i = 0; i < textByLine.length; i++) {
     if (textByLine[i + 1] >= textByLine[i]) counter++;
 }
 
-console.log(counter);
+console.log("Result: " + counter);
 
 // --- Part Two ---
 
@@ -20,17 +20,10 @@ const numbers = fileContent.split('\n').map((el) => +el);
 
 const output =
   numbers.reduce((accu, _, i, array) => (array[i + 3] && array[i] < array[i + 3] ? ++accu : accu), 0);
-//   .reduce((accu, _, i, array) => (array[i + 2] ? [...accu, array[i] + array[i + 1] + array[i + 2]] : accu), [])
-//   .reduce((accu, _, i, array) => (i > 0 && array[i] > array[i - 1] ? ++accu : accu), 0);
 
+console.log("Result: " + output);
 
-console.log(output); // 1344
-
-
-
-
-
-
+// --- --- --- --- --- --- --- --- --- --- --- ---
 // other solution for this problem:
 
 // var fs = require('fs');
