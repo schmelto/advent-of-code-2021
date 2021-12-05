9f91ca01d17d0136a45e7c9470c831cd4ea1aea7
schmelto <30869493+schmelto@users.noreply.github.com>
2021-12-05 13:27:18 +0100

    feat: day04 and day05

---
+++ src/day04/day04.js
@@ -0,0 +1,89 @@
+var fs = require("fs");
+var input = fs.readFileSync("../../input/day04.txt").toString('utf-8').split("\n");
+
+const GRID_SIZE = 5;
+
+var drawnNumbers = input[0].split(",")
+var numOrder = [];
+drawnNumbers.forEach((num, i) => {
+    numOrder[num] = i;
+});
+
+
+var minBoard = [];
+var maxBoard = [];
+var minDrawTime = Number.MAX_SAFE_INTEGER;
+var maxDrawTime = Number.MIN_SAFE_INTEGER;
+
+for (var i = 2; i < input.length; i+= GRID_SIZE + 1){
+    var currentBoard = readBoard(i);
+    var drawTime = calculateBoardDrawTime(currentBoard);
+    
+    if (drawTime < minDrawTime){
+        minDrawTime = drawTime;
+        minBoard = currentBoard;
+    }
+
+    if (drawTime > maxDrawTime){
+        maxDrawTime = drawTime;
+        maxBoard = currentBoard;
+    }
+}
+
+var minResult = calculateResult(minBoard, minDrawTime);
+var maxResult = calculateResult(maxBoard, maxDrawTime);
+
+console.log("PART 1");
+console.log ("Last drawn number: " + drawnNumbers[minDrawTime]);
+console.log("First completed board: " + minBoard);
+console.log("Result: " + minResult);
+console.log("------------");
+console.log("PART 2");
+console.log ("Last drawn number: " + drawnNumbers[maxDrawTime]);
+console.log("Last completed board: " + maxBoard);
+console.log("Result: " + maxResult);
+
+/****
+ * FUNCTIONS
+ */
+
+function readBoard(startIndex){
+    var currentBoard = [];
+    for (var i = 0; i < GRID_SIZE; i++){
+        currentBoard[i] = input[startIndex + i].trim().split(/\s+/).map(el => parseInt(el));
+    }
+
+    return currentBoard;
+}
+
+function calculateBoardDrawTime(board){
+    var boardDrawTimes = board.map( row => row.map( column => column = numOrder[column]));
+
+    var boardDrawTime = Number.MAX_SAFE_INTEGER;
+
+    const checkMax = arr => {
+        var max = Math.max(...arr);
+        if (max < boardDrawTime){
+            boardDrawTime = max;
+        }
+    };
+
+    //check rows
+    boardDrawTimes.forEach(row => {
+        checkMax(row);
+    });
+
+    //check columns
+    for (var i = 0; i < GRID_SIZE; i++){
+        checkMax(boardDrawTimes.map(row => row[i]));
+    }
+    
+    return boardDrawTime;
+}
+
+function calculateResult(board, drawTime){   
+    var unmarkedSum = [].concat(...board).reduce((prev, curr) => numOrder[curr] > drawTime ? prev + curr : prev, 0);
+    var lastDrawnNum = parseInt(drawnNumbers[drawTime]);
+
+    return unmarkedSum * lastDrawnNum;
+}
---
+++ src/day05/day05.js
@@ -0,0 +1,74 @@
+var fs = require("fs");
+var input = fs.readFileSync("../../input/day05.txt").toString('utf-8').split("\n");
+
+console.log("Part 1 solution: " + getOverlapCount(false, input));
+console.log("Part 2 solution: " + getOverlapCount(true, input))
+
+function getOverlapCount(allowDiagonals, lineInput){
+    var pointMap = [];
+
+    lineInput.forEach(rawLine => {
+        var line = parseLine(rawLine);
+        if (!allowDiagonals && isDiagonal(line)){
+            return;
+        }
+    
+        generateLinePoints(line).forEach(point => {
+            if (pointMap[point]){
+                pointMap[point]++;
+            } else {
+                pointMap[point] = 1;
+            }
+        });
+    });
+    
+    //Count overlaps
+    var overlapCount = 0;
+    for (var pointCount in pointMap){
+        if (pointMap[pointCount] > 1){
+            overlapCount++;
+        }
+    }
+
+    return overlapCount;
+}
+
+
+function parseLine(rawLine){
+   return rawLine.split(" -> ").map(el => el.split(",").map(num => parseInt(num)));
+}
+
+function isDiagonal(line) {
+    //check if start and end coordinates are the same on x or y axis
+    return !(line [0][0] == line [1][0] || line [0][1] == line [1][1]);
+}
+
+function generateLinePoints(line){
+    var points = [];
+
+    var linePath = [norm(line[1][0] - line[0][0]),
+                    norm(line[1][1] - line[0][1])];
+    
+    var currPoint = [...line[0]];
+    points.push([...currPoint]);
+    while (!compareArrays(currPoint, line[1])) {
+        currPoint[0] += linePath[0];
+        currPoint[1] += linePath[1];
+        points.push([...currPoint]);
+    }
+
+    return points;
+}
+
+function norm(num){
+    if (num > 0){
+        return 1;
+    } else if (num < 0){
+        return -1;
+    }
+    return 0;
+}
+
+function compareArrays(array1, array2){
+    return array1.length === array2.length && array1.every((value, index) => value === array2[index]);
+}
